662a515703eaf472087e0cc36629aec23dcd745b
ILYA Erygin <ier81@mail.ru>
2020-03-08 11:57:00 +0300

    new function ucFirst(str)

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,4 +4,20 @@
  * @returns {string}
  */
 function ucFirst(str) {
+
+  if (!str)   {
+
+    return str = "";
+
+  }
+
+  if(str.length === 1) {
+
+    return str = str[0].toUpperCase();
+
+  } else {
+
+    return str = str[0].toUpperCase() + str.slice(1);
+
+  }
 }
