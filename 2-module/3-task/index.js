2a38549752cb31bbc89fb3822311586cc084633e
ILYA Erygin <ier81@mail.ru>
2020-03-12 19:40:55 +0300

    Add calculator Object

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,27 @@
 let calculator = {
-  // ваш код
+
+  read (a,b) {
+    //a = +prompt ("Введите первое число: ", "");
+    //b = +prompt ("Введите второе число: ", "");
+    this.a = a;
+    this.b = b;
+
+  },
+
+  sum () {
+
+    return sum = + this.a + + this.b
+    this.sum = sum;
+
+  },
+
+  mul () {
+
+    return mul = this.a * this.b;
+    this.mul = mul;
+
+  },
+
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
