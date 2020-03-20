4e5cbbb060e11f671c1c4c1c41ab7bcd5af17ad7
ILYA Erygin <ier81@mail.ru>
2020-03-20 09:07:26 +0300

    Задача №5 модуля №3 функция фильтрует массив по заданному диапазону и возвращает новый массив не изменяя старый

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,27 @@
  * @param {number} b
  * @returns {number[]}
  */
+
+
+
+/* Задача №5 модуля №3 функция фильтрует массив по заданному 
+диапазону и возвращает новый массив не изменяя старый*/
+
+
 function filterRange(arr, a, b) {
+
+	let arr1=[];
+
+	let result = arr.filter( (item) => {
+    if((item > a || item==a) && (item < b || item==b))  
+
+    return arr1.push(item);
+  })
+
+  return arr1;
 }
+
+
+
+
+
