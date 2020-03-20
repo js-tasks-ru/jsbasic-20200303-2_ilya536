d53c075954d84419f66fce78513e487ef9879acb
ILYA Erygin <ier81@mail.ru>
2020-03-20 16:23:18 +0300

    вставка списка элементов на страницу

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -3,5 +3,32 @@
  * @param {Object[]} friends
  * @return {HTMLUListElement}
  */
+
+
 function makeFriendsList(friends) {
-}
+
+
+let ul = document.createElement('ul');
+
+
+	 for(let elem of friends) {
+
+		let liFullName = document.createElement('li');
+
+	 	liFullName.innerHTML = `${elem.firstName}  ${elem.lastName}`;
+
+	 	ul.append(liFullName);
+	 }
+	 	
+	return ul;
+	}
+
+
+
+
+
+
+	
+
+
+
