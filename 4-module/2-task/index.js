75786fc60b5138b7e21a453f4d6413f01f2c6a76
ILYA Erygin <ier81@mail.ru>
2020-03-22 14:38:50 +0300

    Модуль 4 Задача 2. Окрашивание диагональных ячеек таблицы

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,15 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+
+
+const trVar = table.rows;
+let elem;
+for (let i=0 ; i<trVar.length; i++) {
+
+	elem = trVar[i].cells[i].style.backgroundColor = 'red';
+
+	 }
+return elem;
 }
+
